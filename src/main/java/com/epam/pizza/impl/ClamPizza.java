69343a9eb7ca72e5e8036ac5d082b0e9c4a90fbc
Andriy <andriymattt@gmail.com>
2019-12-22 23:47:21 +0200

    feature/factory: add ClamPizza class

---
+++ src/main/java/com/epam/pizza/impl/ClamPizza.java
@@ -0,0 +1,57 @@
+package com.epam.pizza.impl;
+
+import com.epam.City;
+import com.epam.pizza.DoughType;
+import com.epam.pizza.Pizza;
+import com.epam.pizza.SauceType;
+import com.epam.pizza.Topping;
+
+import java.util.ArrayList;
+
+public class ClamPizza extends AbstractPizza implements Pizza {
+
+    public ClamPizza() {
+        toppings = new ArrayList<>();
+    }
+
+    @Override
+    public void prepare(City city) {
+        if (city == City.Lviv) {
+            toppings.add(Topping.Cheddar);
+            toppings.add(Topping.Ham);
+            toppings.add(Topping.Clams);
+            toppings.add(Topping.Mushrooms);
+            toppings.add(Topping.Tomatoes);
+            toppings.add(Topping.Pepper);
+            doughType = DoughType.Thin;
+            sauceType = SauceType.Pesto;
+        } else if (city == City.Kyiv) {
+            toppings.add(Topping.Parmesan);
+            toppings.add(Topping.Sausage);
+            toppings.add(Topping.Clams);
+            toppings.add(Topping.Tomatoes);
+            toppings.add(Topping.Olives);
+            doughType = DoughType.Thin;
+            sauceType = SauceType.PlumTomato;
+        } else if (city == City.Dnipro) {
+            toppings.add(Topping.Mozzarella);
+            toppings.add(Topping.Ham);
+            toppings.add(Topping.Clams);
+            toppings.add(Topping.Mushrooms);
+            toppings.add(Topping.Onion);
+            toppings.add(Topping.Tomatoes);
+            doughType = DoughType.Thick;
+            sauceType = SauceType.Marinara;
+        }
+        System.out.println("The pizza has been prepared!");
+    }
+
+    @Override
+    public String toString() {
+        return "ClamPizza{" +
+                "toppings=" + toppings +
+                ", doughType=" + doughType +
+                ", sauceType=" + sauceType +
+                "}";
+    }
+}
