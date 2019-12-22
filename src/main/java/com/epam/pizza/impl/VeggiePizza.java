89c86cdce8534b222e13a12fc7ec861659dcf727
Andriy <andriymattt@gmail.com>
2019-12-22 23:38:16 +0200

    feature/factory: add VeggiePizza class

---
+++ src/main/java/com/epam/pizza/impl/VeggiePizza.java
@@ -0,0 +1,51 @@
+package com.epam.pizza.impl;
+
+import com.epam.City;
+import com.epam.pizza.DoughType;
+import com.epam.pizza.Pizza;
+import com.epam.pizza.SauceType;
+import com.epam.pizza.Topping;
+
+import java.util.ArrayList;
+
+public class VeggiePizza extends AbstractPizza implements Pizza {
+
+    public VeggiePizza() {
+        toppings = new ArrayList<>();
+    }
+
+    @Override
+    public void prepare(City city) {
+        if (city == City.Lviv) {
+            toppings.add(Topping.Mozzarella);
+            toppings.add(Topping.Mushrooms);
+            toppings.add(Topping.Tomatoes);
+            toppings.add(Topping.Pepper);
+            doughType = DoughType.Thin;
+            sauceType = SauceType.PlumTomato;
+        } else if (city == City.Kyiv) {
+            toppings.add(Topping.Cheddar);
+            toppings.add(Topping.Tomatoes);
+            toppings.add(Topping.Onion);
+            doughType = DoughType.Thin;
+            sauceType = SauceType.Marinara;
+        } else if (city == City.Dnipro) {
+            toppings.add(Topping.Parmesan);
+            toppings.add(Topping.Mushrooms);
+            toppings.add(Topping.Cucumbers);
+            toppings.add(Topping.Onion);
+            doughType = DoughType.Thick;
+            sauceType = SauceType.Pesto;
+        }
+        System.out.println("The pizza has been prepared!");
+    }
+
+    @Override
+    public String toString() {
+        return "VeggiePizza{" +
+                "toppings=" + toppings +
+                ", doughType=" + doughType +
+                ", sauceType=" + sauceType +
+                "}";
+    }
+}
