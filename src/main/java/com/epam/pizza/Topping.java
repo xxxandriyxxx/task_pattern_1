ed8336ef42b86c3aa7d8ab1559b300defe27ea81
Andriy <andriymattt@gmail.com>
2019-12-22 20:04:36 +0200

    feature/factory: add Topping enum

---
+++ src/main/java/com/epam/pizza/Topping.java
@@ -0,0 +1,8 @@
+package com.epam.pizza;
+
+public enum Topping {
+    Mozzarella, Parmesan, Cheddar,
+    Sausage, Ham, Pepperoni,
+    Tomatoes, Pepper, Cucumbers, Olives, Onion,
+    Mushrooms, Clams
+}
